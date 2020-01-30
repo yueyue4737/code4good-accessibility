ef6e0bb794404f9960bd230a84accdda96e00bee
Annah Patterson <annah.pelin@gmail.com>
2020-01-29 18:20:34 -0800

    Adds scan results for C4G-153 CD-1-4 + CD-44

--- scan-results/results_ticketnumber_example/analyze.results.js
+++ scan-results/results_ticketnumber_example/analyze.results.js
@@ -3,6 +3,8 @@ const data = require('./results.ticketnumber.sampleTest.js');
 data.forEach((obj, err) => {
   data.analyze(obj, (err, data) => {
     if (err) { console.log('error in retrieving records', err); }
-    else { console.log('retrieved records', data); }
+    else {
+      console.log('retrieved records', data);
+    }
   });
 });
