4fa472088322cab2dcd79e642f32f0b8ad3fadec
Rachel Bitting <rachel.bitting@gmail.com>
2020-02-14 12:15:51 -0500

    Updates

---
+++ precache-manifest.4fa7ce27adb894fd7146a11e02c0ff8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c006ae98f2c63ef41d82e150e036ee5d",
+    "url": "/code4good-accessibility/index.html"
+  },
+  {
+    "revision": "2e4ccb481f4aee32dcef",
+    "url": "/code4good-accessibility/static/css/main.e9471b9e.chunk.css"
+  },
+  {
+    "revision": "7e7466da3652073ce213",
+    "url": "/code4good-accessibility/static/js/2.981a7b77.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/code4good-accessibility/static/js/2.981a7b77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e4ccb481f4aee32dcef",
+    "url": "/code4good-accessibility/static/js/main.cf59d88d.chunk.js"
+  },
+  {
+    "revision": "6f3cfaab21474aa572e2",
+    "url": "/code4good-accessibility/static/js/runtime-main.06f47c70.js"
+  }
+]);
