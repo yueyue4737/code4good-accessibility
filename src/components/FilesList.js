fa04e18173b0c00866ec52a9a1a89ed7235538b7
Rachel Bitting <rachel.bitting@gmail.com>
2020-02-14 12:02:50 -0500

    WIP fixed url I broke

--- src/components/FilesList.js
+++ src/components/FilesList.js
@@ -24,7 +24,7 @@ class FilesList extends React.Component {
                 sortAsc: false
             },
             fetchComplete: false,
-            url: "https://api.github.com/repos/annahinnyc/code4good-accessibility/contents/public/scan-results/data-chrome-dev"
+            url: "https://api.github.com/repos/annahinnyc/code4good-accessibility/contents/scan-results/data-chrome-dev"
             //url: "https://api.github.com/repos/rbitting/testing/contents/audits"
         }
         this.sortNestedItems = this.sortNestedItems.bind(this);
