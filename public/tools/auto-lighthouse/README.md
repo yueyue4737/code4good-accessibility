092a4b23eb061931a91a33d9fe19a50c9159d0a0
Jason Winerip <quser4574@gmail.com>
2020-05-04 16:15:43 -0700

    Add flags to the script to address certain error cases in excel files

--- public/tools/auto-lighthouse/README.md
+++ public/tools/auto-lighthouse/README.md
@@ -8,9 +8,14 @@ you will need to install the following packages:
 
 npm install lighthouse
 npm install exceljs
+npm install flags
 
 ## Run the script
 
-The script expects two arguments - the absolute path to your excel file and the prefix to use for your output files of the form `C4G_##_CD_`
+The script requires two flags - the absolute path to your excel file and the prefix to use for your output files of the form `C4G_##_CD_`
 
-In the base folder (same level as `package.json`), run `npm run lighthouse filepath output_prefix`. For example: `npm run lighthouse "c:/Users/User/Downloads/RCB Home B24.xlsx" C4G_00_CD_`
+In the base folder (same level as `package.json`), run `npm run lighthouse -- --inputFile="filepath" --outputPrefix=output_prefix`. For example: `npm run lighthouse  -- --inputFile="c:/Users/User/Downloads/RCB Home B24.xlsx" --outputPrefix=C4G_00_CD_`
+
+Additionally, if your excel file has issues, you can provide flags for which line column headers are on (headerStart), how many rows below the header the first URL appears (urlOffset), or which worksheet number the URLs are on (worksheet).
+
+For example, if your excel file has an extra worksheet, and you need to run on worksheet 2, you would run `npm run lighthouse  -- --inputFile="c:/Users/User/Downloads/RCB Home B24.xlsx" --outputPrefix=C4G_00_CD_ --worksheet=2`
--- public/tools/auto-lighthouse/index.js
+++ public/tools/auto-lighthouse/index.js
@@ -6,6 +6,13 @@ const fs = require("fs");
 const Excel = require("exceljs");
 const lighthouse = require("lighthouse");
 const chromeLauncher = require("chrome-launcher");
+var flags = require('flags');
+
+flags.defineString('inputFile', '', 'Excel file to read');
+flags.defineString('outputPrefix', '', 'JSON file prefix');
+flags.defineInteger('headerStart', 1, 'Row number on which the header including the url label is located');
+flags.defineInteger('urlOffset', 1, 'How many rows after the url label the first url appears (1 for next row)');
+flags.defineInteger('worksheet', 1, 'which worksheet to read from, if multiple sheets present');
 
 const RESULTS_FOLDER = "./public/scan-results/data";
 
@@ -20,19 +27,23 @@ function launchChromeAndRunLighthouse(url, opts, config = null) {
     });
 }
 
-const readWorkbook = async (filepath, resultsPrefix) => {
+const readWorkbook = async (filepath, resultsPrefix, headerRowNumber, UrlRowOffsetFromHeader, worksheetNumber) => {
   const startTime = Date.now();
+  const UrlStartRowNumber = headerRowNumber + UrlRowOffsetFromHeader;
+  const IndexOffsetForNaming = UrlStartRowNumber - 1;
 
-  console.log("Reading xlsx file...");
+  console.log("Header expected on row "+headerRowNumber);
+  console.log("URLs expected offset from header by "+ UrlRowOffsetFromHeader);
+  console.log("Reading xlsx file from " + filepath);
   const workbook = new Excel.Workbook();
   await workbook.xlsx.readFile(filepath);
-  const worksheet = workbook.getWorksheet(1);
+  const worksheet = workbook.getWorksheet(worksheetNumber);
   // Find the url column
   var columnHeader = "";
   var columnNumber = 0;
   while (columnHeader != "url" && columnNumber<worksheet.columnCount) {
     columnNumber++;
-    columnHeader = worksheet.getRow(1).getCell(columnNumber).value;
+    columnHeader = worksheet.getRow(headerRowNumber).getCell(columnNumber).value;
   }
   if (columnHeader == null) {
     console.error('Couldn\'t find the url column');
@@ -41,11 +52,11 @@ const readWorkbook = async (filepath, resultsPrefix) => {
   }
   const urlCol = worksheet.getColumn(columnNumber);
 
-  for (let rowNumber = 2; rowNumber < urlCol.values.length; rowNumber++) {
+  for (let rowNumber = UrlStartRowNumber; rowNumber < urlCol.values.length; rowNumber++) {
     const url = urlCol.values[rowNumber];
     if (!url) continue;
 
-    console.log(`Auditing index ${rowNumber - 1}: ${url}`);
+    console.log(`Auditing index ${rowNumber - IndexOffsetForNaming}: ${url}`);
     const results = await launchChromeAndRunLighthouse(url, {
       // See lighthouse docs for configuration values. Adjust as needed.
       // https://github.com/GoogleChrome/lighthouse/blob/master/docs/configuration.md
@@ -54,10 +65,10 @@ const readWorkbook = async (filepath, resultsPrefix) => {
       emulatedFormFactor: "desktop",
     });
 
-    const resultsFileName = `${RESULTS_FOLDER}/${resultsPrefix}${rowNumber - 1}.json`;
+    const resultsFileName = `${RESULTS_FOLDER}/${resultsPrefix}${rowNumber - IndexOffsetForNaming}.json`;
     fs.writeFile(resultsFileName, results, (err) => {
       console.log(
-        `Wrote result file for index ${rowNumber - 1}: ${resultsFileName}`
+        `Wrote result file for index ${rowNumber - IndexOffsetForNaming}: ${resultsFileName}`
       );
       err && console.error(err);
     });
@@ -70,7 +81,5 @@ const readWorkbook = async (filepath, resultsPrefix) => {
 
 // Configure the first argument to be the location of the xlsx file (relative to package.json).
 // Configure the second argument to be the prefix of the result json files.
-var myArgs = process.argv.slice(2);
-let ssFile = myArgs[0];
-let resultFile = myArgs[1];
-readWorkbook(ssFile, resultFile);
+flags.parse();
+readWorkbook(flags.get('inputFile'), flags.get('outputPrefix'), flags.get('headerStart'), flags.get('urlOffset'), flags.get('worksheet'));
